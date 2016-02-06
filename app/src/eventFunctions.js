4fa5eb4b1e332384bbf7ea059ccadff1c909e259
Raif Harik <reharik@gmail.com>
2016-02-06 12:10:19 -0600

    adding uuid

--- app/src/eventFunctions.js
+++ app/src/eventFunctions.js
@@ -1,5 +1,5 @@
 
-module.exports = function(R, functionalHelpers){
+module.exports = function(R, uuid, functionalHelpers){
     var fh = functionalHelpers;
     var parseMetadata = R.compose(R.chain(fh.parseBuffer), R.chain(fh.safeProp('Metadata')), fh.safeProp('OriginalEvent'));
     var parseData = R.compose(R.chain(fh.parseBuffer), R.chain(fh.safeProp('Data')), fh.safeProp('OriginalEvent'));
