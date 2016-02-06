4a0ccfa2ed4171ee2bf755e931d78052aa14c8c8
Raif Harik <reharik@gmail.com>
2016-02-06 13:59:04 -0600

    more errors:

--- app/src/eventFunctions.js
+++ app/src/eventFunctions.js
@@ -1,8 +1,8 @@
 
 module.exports = function(R, uuid, functionalHelpers){
     var fh = functionalHelpers;
-    var parseMetadata = R.compose(R.chain(fh.parseBuffer), R.chain(fh.safeProp('Metadata')), fh.safeProp('OriginalEvent'));
-    var parseData = R.compose(R.chain(fh.parseBuffer), R.chain(fh.safeProp('Data')), fh.safeProp('OriginalEvent'));
+    var parseMetadata = R.compose(R.chain(fh.safeParseBuffer), R.chain(fh.safeProp('Metadata')), fh.safeProp('OriginalEvent'));
+    var parseData = R.compose(R.chain(fh.safeParseBuffer), R.chain(fh.safeProp('Data')), fh.safeProp('OriginalEvent'));
     var outGoingEvent = event => {
         return {
             EventId : uuid.v4(),
