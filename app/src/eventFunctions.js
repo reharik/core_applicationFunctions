931a10feb82dd67db1d73e7917697e24bd92e72e
Raif Harik <reharik@gmail.com>
2016-02-06 11:49:36 -0600

    updating with new funcs

--- app/src/eventFunctions.js
+++ app/src/eventFunctions.js
@@ -3,16 +3,17 @@ module.exports = function(R, functionalHelpers){
     var fh = functionalHelpers;
     var parseMetadata = R.compose(R.chain(fh.parseBuffer), R.chain(fh.safeProp('Metadata')), fh.safeProp('OriginalEvent'));
     var parseData = R.compose(R.chain(fh.parseBuffer), R.chain(fh.safeProp('Data')), fh.safeProp('OriginalEvent'));
-    var outGoingEvent = R.curry((event, metadata) => {
+    var outGoingEvent = event => {
         return {
             EventId : uuid.v4(),
             Type : fh.safeProp('eventName',event).getOrElse(''),
             IsJson : true,
-            Data    : new buffer.Buffer(JSON.stringify(fh.safeProp('data',event).getOrElse(''))),
-            Metadata:  new buffer.Buffer(JSON.stringify(metadata))
-        };
-    });
-    var incomingEvent = event=> {
+            Data    : fh.safeCreateBuffer(fh.safeProp('data',event)).getOrElse(),
+            Metadata:  fh.safeCreateBuffer(fh.safeProp('metadata',event)).getOrElse()
+        }
+    };
+
+    var incomingEvent = event => {
         return {
             eventName : (R.compose(R.chain(fh.safeProp('eventName')),parseMetadata)(event)).getOrElse(''),
             metadata: parseMetadata(event).getOrElse({}),
--- app/src/functionalHelpers.js
+++ app/src/functionalHelpers.js
@@ -3,67 +3,102 @@
  */
 
 module.exports = function(R, _fantasy, buffer) {
-var Maybe = _fantasy.Maybe;
-var safeProp = R.curry((x,o) => o ? Maybe(o[x]) : Maybe.Nothing() );
-var startsWith  = R.curry((x,s) => s.startsWith(x) );
-var parseBuffer = x => buffer.Buffer.isBuffer(x) ? tryParseJSON(x.toString('utf8')) : Maybe.Nothing() ;
-var tryParseJSON = x => { try { return Maybe.of(JSON.parse(x)); }
-                        catch (e) { return Maybe.Nothing(); } };
-var log     = (x) => {
-    console.log('==========log=========');
-    console.log(x);
-    console.log('==========ENDlog=========');
-    return x;
-};
-var logPlus     = R.curry((y,x) => {
-    console.log('==========log '+y+'=========');
-    console.log(x);
-    console.log('==========ENDlog '+y+'=========');
-    return x;
-});
-var logForkPlus = R.curry((y,x)  => {
-    var fr, sr;
-    x.fork(f=> {
-            console.log('==========log failure ' + y + '=========');
-            console.log(f);
-            console.log('==========ENDlog failure ' + y + '=========');
-            fr = f;
-        },
-            s=> {
-            console.log('==========log success ' + y + '=========');
-            console.log(s);
-            console.log('==========ENDlog success ' + y + '=========');
-            sr = s;
-        });
+    var Maybe           = _fantasy.Maybe;
 
-    return sr ? Future((rej, res)=> res(sr)) : Future((rej, res)=> rej(fr));
-});
-var logFork = x  => {
-    var fr, sr;
-    x.fork(f=> {
-            console.log('==========log failure=========');
-            console.log(f);
-            console.log('==========ENDlog failure=========');
-            fr = f;
-        },
-            s=> {
-            console.log('==========log success=========');
-            console.log(s);
-            console.log('==========ENDlog success=========');
-            sr = s;
-        });
+    var safeProp        = R.curry((x, o) => o ? Maybe(o[x]) : Maybe.Nothing());
 
-    return sr ? Future((rej, res)=> res(sr)) : Future((rej, res)=> rej(fr));
-};
+    var startsWith      = R.curry((x, s) => s.startsWith(x));
 
-return {
-    safeProp,
-    startsWith,
-    parseBuffer,
-    tryParseJSON,
-    log,
-    logPlus,
-    logFork,
-    logForkPlus
-}
+    var getSafeValue = (src, prop, _default) => {
+        if(_default) {
+            return safeProp(src, prop).getOrElse(_default);
+        }
+        return safeProp(src, prop).getOrElse();
+    };
+
+    var safeParseBuffer = x => buffer.Buffer.isBuffer(x) ? tryParseJSON(x.toString('utf8')) : Maybe.Nothing();
+
+    var safeCreateBuffer = x => {
+        var val = x.getOrElse('') || x;
+        var buff = new buffer.Buffer(tryStringify(val).getOrElse(''));
+        return buff ? Maybe.of(buff) : Maybe.Nothing();
+    };
+
+    var tryParseJSON = x => {
+        try {
+            return Maybe.of(JSON.parse(x));
+        }
+        catch (e) {
+            return Maybe.Nothing();
+        }
+    };
+    var tryStringify = x => {
+        try {
+            return Maybe.of(JSON.stringify(x));
+        }
+        catch (e) {
+            return Maybe.Nothing();
+        }
+    };
+
+    var log         = (x) => {
+        console.log('==========log=========');
+        console.log(x);
+        console.log('==========ENDlog=========');
+        return x;
+    };
+    var logPlus     = R.curry((y, x) => {
+        console.log('==========log ' + y + '=========');
+        console.log(x);
+        console.log('==========ENDlog ' + y + '=========');
+        return x;
+    });
+    var logForkPlus = R.curry((y, x)  => {
+        var fr, sr;
+        x.fork(f=> {
+                console.log('==========log failure ' + y + '=========');
+                console.log(f);
+                console.log('==========ENDlog failure ' + y + '=========');
+                fr = f;
+            },
+                s=> {
+                console.log('==========log success ' + y + '=========');
+                console.log(s);
+                console.log('==========ENDlog success ' + y + '=========');
+                sr = s;
+            });
+
+        return sr ? Future((rej, res)=> res(sr)) : Future((rej, res)=> rej(fr));
+    });
+    var logFork     = x  => {
+        var fr, sr;
+        x.fork(f=> {
+                console.log('==========log failure=========');
+                console.log(f);
+                console.log('==========ENDlog failure=========');
+                fr = f;
+            },
+                s=> {
+                console.log('==========log success=========');
+                console.log(s);
+                console.log('==========ENDlog success=========');
+                sr = s;
+            });
+
+        return sr ? Future((rej, res)=> res(sr)) : Future((rej, res)=> rej(fr));
+    };
+
+    return {
+        safeProp,
+        startsWith,
+        getSafeValue,
+        safeParseBuffer,
+        safeCreateBuffer,
+        tryParseJSON,
+        tryStringify,
+        log,
+        logPlus,
+        logFork,
+        logForkPlus
+    }
 };
