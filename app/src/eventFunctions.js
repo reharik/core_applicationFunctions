b30801f9f798ce2ca8e255350ec6e4ba21d463f9
Raif Harik <reharik@gmail.com>
2016-02-28 20:56:38 -0600

    switching it back

--- app/src/eventFunctions.js
+++ app/src/eventFunctions.js
@@ -1,18 +1,15 @@
 
-module.exports = function(R, uuid, _fantasy, functionalHelpers){
+module.exports = function(R, uuid, functionalHelpers){
     var fh = functionalHelpers;
     var parseMetadata = R.compose(R.chain(fh.safeParseBuffer), R.chain(fh.safeProp('Metadata')), fh.safeProp('Event'));
     var parseData = R.compose(R.chain(fh.safeParseBuffer), R.chain(fh.safeProp('Data')), fh.safeProp('Event'));
     var outGoingEvent = event => {
-        var data = fh.safeProp('data',event);
-        var _metadata = (e,d) =>  _fantasy.Maybe(R.merge(fh.safeProp('metadata',e), fh.safeProp('continuationId',d)));
-        var metadata = _metadata(data,event);
         return {
             EventId : uuid.v4(),
             Type : fh.safeProp('eventName',event).getOrElse(''),
             IsJson : true,
-            Data    : fh.safeCreateBuffer(data).getOrElse(),
-            Metadata:  fh.safeCreateBuffer(metadata).getOrElse()
+            Data    : fh.safeCreateBuffer(fh.safeProp('data',event)).getOrElse(),
+            Metadata:  fh.safeCreateBuffer(fh.safeProp('metadata',event)).getOrElse()
         }
     };
 
